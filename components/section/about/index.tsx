06ffa04464ebfdcf1b3b522e932848bb3caebd8f
Rafael Stuepp Riegel <rafaelsriegel@gmail.com>
2023-03-01 03:19:28 -0300

    Curriculum Vitae

--- components/section/about/index.tsx
+++ components/section/about/index.tsx
@@ -132,6 +132,7 @@ const About: React.FC = () => {
               </button>
               Curriculum Vitae
             </Button>
+            
           </Link>
           <Link
             isExternal
