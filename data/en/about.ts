eeea0c4bce9d8796ebf7295abae46cf8eb75b26c
Rafael Stuepp Riegel <rafaelsriegel@gmail.com>
2023-03-01 05:04:55 -0300

    Update about.ts

--- data/en/about.ts
+++ data/en/about.ts
@@ -2,7 +2,7 @@ import { AboutInterface } from "../../components/section/about/types"
 
 const about: AboutInterface = {
   name: 'Rafael Stuepp Riegel',
-  description: `Portfolio & Curriculum Vitae de Rafael Stuepp Riegel`,
+  description: `Portfolio & Curriculum Vitae de Rafael Stuepp Riegel\n(LandingPage em Desenvolvimento)`,
   email: 'rafaelsriegel@gmail.com'
 }
 
